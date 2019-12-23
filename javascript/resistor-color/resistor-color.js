ecec39212c2296b685512267193d77fe87a9bc66
sunlee <sunlee128@gmail.com>
2019-12-23 19:14:14 +1100

    ruby track added

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,24 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const colorMap = {
+ 'black': 0,
+ 'brown': 1,
+ 'red': 2,
+ 'orange': 3,
+ 'yellow': 4,
+ 'green': 5,
+ 'blue': 6,
+ 'violet': 7,
+ 'grey': 8,
+ 'white': 9
+}
+export const colorCode = (color) => {
+if (color){
+  return colorMap[color]
+} 
+};
+
+export const COLORS = Object.keys(colorMap);
--- ruby/hello-world/hello_world.rb
+++ ruby/hello-world/hello_world.rb
@@ -5,4 +5,10 @@ Write your code for the 'Hello World' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/hello-world` directory.
 =end
+class HelloWorld
+  def hello
+    "Hello, World!"
+  end 
+end
 
+HelloWorld = HelloWorld.new
