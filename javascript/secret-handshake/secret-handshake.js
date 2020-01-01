b0de9a1e25385ff84d047ff283064a716078d423
sunlee <sunlee128@gmail.com>
2020-01-01 15:23:57 +0200

    secrete handshake

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -3,6 +3,26 @@
 // convenience to get you started writing code faster.
 //
 
-export const secretHandshake = () => {
-  throw new Error("Remove this statement and implement this function");
+export const secretHandshake = (number) => {
+  if(isNaN(number)) throw new Error('Handshake must be a number');
+  
+  let result = [];
+  let reverse = (number & 16) ;
+  if (number & 1) result.push("wink");
+  if (number & 2) result.push("double blink");
+  if (number & 4) result.push("close your eyes");
+  if (number & 8) result.push("jump");
+  if(reverse) {
+    return result.reverse() 
+  } else {
+    return result;
+  }
 };
+
+// 1 = wink
+// 10 = double blink
+// 100 = close your eyes
+// 1000 = jump
+
+
+// 10000 = Reverse the order of the operations in the secret handshake
